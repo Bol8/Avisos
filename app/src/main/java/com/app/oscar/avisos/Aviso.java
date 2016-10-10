e0c2a0c0815a7c84dc3e19fe99d4ec8d624b5e67
Bol8 <oscarbolon@hotmail.com>
2016-10-10 16:47:25 +0200

    Creada la clase Aviso. Creada la clase AvisosDBAdapter.

---
+++ app/src/main/java/com/app/oscar/avisos/Aviso.java
@@ -0,0 +1,44 @@
+package com.app.oscar.avisos;
+
+/**
+ * Created by Oscar on 10/10/2016.
+ */
+
+public class Aviso {
+
+    private int mId;
+    private String mContent;
+    private int mImportant;
+
+
+    public Aviso(int id, String content, int important) {
+        mId = id;
+        mContent = content;
+        mImportant = important;
+    }
+
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
+
+    public int getImportant() {
+        return mImportant;
+    }
+
+    public void setImportant(int important) {
+        mImportant = important;
+    }
+}
